f552f6793f49da7fd3402b885b114bce06ef11b8
Mujassir Nasir <mnasir@xavor.com>
2020-12-10 03:25:34 +0500

    - A new page with search functionality that can search the companies and persons across all the lists (e.g. existing, future created) - http://davidfogg00.web809.discountasp.net/GlobalSearch/Index - The result set will display the list of companies and contacts - In the company result set -- Name has a link to open the company's page (PROSPECT VIEW COMPANY) -- Edit button to open company record in the edit view (PROSPECT ADD/EDIT COMPANY (ADMIN)) - In the contact result set -- All details are given in the first column (Name, E-mail, LinkedIn Profile, etc.) -- Notes in the 2nd column -- View company details link in the third column that opens its company's page (PROSPECT VIEW COMPANY) - Count of records are displayed in the (companies and contacts) headers - Both the headers (companies and contacts) are clickable and can expand/collapse their respective result set

---
+++ StaffandTrain/Controllers/GlobalSearchController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using StaffandTrain.Common;
+using StaffandTrain.DataModel;
+using StaffandTrain.Models;
+
+namespace StaffandTrain.Controllers
+{
+    [NoCache]
+    [Authorize(Roles = "Admin")]
+    public class GlobalSearchController : Controller
+    {
+        // GET: GlobalSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetSearchResults(string searchQuery)
+        {
+            SATConn context = new SATConn();
+            try
+            {
+                var companySearchQuery = "EXEC DBO.SearchCompany @SearchQuery = '" + searchQuery + "'";
+                var contactSearchQuery = "EXEC DBO.SearchContact @SearchQuery = '" + searchQuery + "'";
+                var companies = context.Database.SqlQuery<Company>(companySearchQuery);
+                var contacts = context.Database.SqlQuery<Contact>(contactSearchQuery);
+                var result = new
+                {
+                    success = true,
+                    companies = companies,
+                    contacts = contacts
+                };
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+
+                var result = new
+                {
+                    success = false,
+                    error = ex
+                };
+                return Json(result);
+            }
+
+        }
+
+
+    }
+}
---
+++ StaffandTrain/Custom_JS/GlobalSearch_Index.js
@@ -0,0 +1,173 @@
+﻿
+
+
+$(document).ready(function () {
+    $('#txtSearch').keypress(function (e) {
+        if (e.which == 13) {
+            loadSearchResults($('#txtSearch').val());
+            return false;    //<---- Add this line
+        }
+    });
+    $('#txtSearch').keyup(function (e) {
+        if ($('#txtSearch').val().length > 0) {
+            $(".search-icon i.search").addClass("hidden");
+            $(".search-icon i.clear").removeClass("hidden");
+        }
+        else {
+            $(".search-icon i.search").removeClass("hidden");
+            $(".search-icon i.clear").addClass("hidden");
+        }
+    });
+
+    $(document).on("click", ".search-icon i.clear", function (e) {
+        $('#txtSearch').val("");
+        $("#tblCompanyResults,#tblContactResults").addClass('hidden');
+    });
+
+    $("#btnSearch").click(function (e) {
+        loadSearchResults($('#txtSearch').val());
+    });
+    $(".company-header").click(function () {
+        if ($(".company-header").hasClass("expanded")) {
+            $("#tblCompanyResults thead tr:last-child").hide();
+            $("#tblCompanyResults tbody").hide();
+            $(".company-ec-icon").html('<i class="fa fa-angle-down"></i>');
+            $(".company-header").removeClass("expanded")
+        }
+        else {
+            $("#tblCompanyResults thead tr").show();
+            $("#tblCompanyResults tbody").show();
+            $(".company-ec-icon").html('<i class="fa fa-angle-up"></i>');
+            $(".company-header").addClass("expanded")
+        }
+    });
+
+    $(".contact-header").click(function () {
+        if ($(".contact-header").hasClass("expanded")) {
+            $("#tblContactResults thead tr:last-child").hide();
+            $("#tblContactResults tbody").hide();
+            $(".contact-ec-icon").html('<i class="fa fa-angle-down"></i>');
+            $(".contact-header").removeClass("expanded")
+        }
+        else {
+            $("#tblContactResults thead tr").show();
+            $("#tblContactResults tbody").show();
+            $(".contact-ec-icon").html('<i class="fa fa-angle-up"></i>');
+            $(".contact-header").addClass("expanded")
+        }
+    });
+});
+
+
+function loadSearchResults(searchQuery) {
+
+    showLoader();
+
+    $.ajax({
+        type: "POST",
+        url: '/GlobalSearch/GetSearchResults',
+        data: { searchQuery: searchQuery },
+        beforeSend: function () { showLoader(); },
+        success: function (response) {
+            hideLoader();
+            console.log('Ajax response ', response);
+            if (response.success) {
+                renderSearchResults(response);
+                $("#tblCompanyResults,#tblContactResults").removeClass('hidden');
+            }
+        },
+        error: function (error) {
+            hideLoader();
+        },
+    });
+}
+
+function renderSearchResults(response) {
+    let companies = response.companies;
+    let contacts = response.contacts;
+    $("#tblCompanyResults tbody, #tblContactResults tbody").html('');
+    if (companies && companies.length > 0) {
+        $(".company-count").html(companies.length);
+        for (var i = 0; i < companies.length; i++) {
+            let element = companies[i];
+            $("#tblCompanyResults tbody").append(prepareCompanyRow(element));
+        }
+    }
+    else {
+        $(".company-count").html(0);
+        $("#tblCompanyResults tbody").append(getBlankRow(3));
+    }
+
+    if (contacts && contacts.length > 0) {
+        $(".contact-count").html(contacts.length);
+        for (var i = 0; i < contacts.length; i++) {
+            let element = contacts[i];
+            $("#tblContactResults tbody").append(prepareContactRow(element));
+        }
+    }
+    else {
+        $(".contact-count").html(0);
+        $("#tblContactResults tbody").append(getBlankRow(3));
+    }
+}
+
+
+
+function prepareCompanyRow(element) {
+    let actions = `<a href="/ProspectViewList/SaveCompany?companyidedit=${element.companyid}" target='_blank' class="details AnyAction"> 
+                                <i class="fa fa-pencil-square-o"></i>
+                               </a>`;
+    let row = `<tr>
+                            <td>${element.biztype ? element.biztype : ''}</td>
+                            <td> <a href="/ProspectViewCompany/Index?Compid=${element.companyid}" target="_blank">
+                                    ${element.name}
+                                </a>
+                            </td>
+                            <td>${actions}</td>
+                           </tr>`;
+
+    return row;
+}
+
+function prepareContactRow(element) {
+    let actions = `<a href="/ProspectViewCompany/Index?Compid=${element.companyid}" target='_blank' class="details AnyAction"> 
+                                View Company Details
+                               </a>`;
+    let row = `<tr>
+                  <td>
+                        <p>ID: <b>${element.contactid}</b></p>
+                        <p>Name: <b>${getValue(element.contactfullname)}</b></p>
+                        <p>Title: <b>${getValue(element.titlestandard)}</b></p>
+                        <p>WPhone: <b>${getValue(element.contactphone)}</b></p>
+                        <p>CPhone: <b>${getValue(element.contactcellphone)}</b></p>
+                        <p>Email: <a href="mailto:${element.contactemail}">${getValue(element.contactemail)}</a></p>
+                        <p>LinkedIn: <a target="_blank" href='${element.linkedinprofileurl}'>${element.linkedinprofileurl ? getValue(element.contactfullname) : ''}</a></p>
+                  </td>
+                  <td>
+                      ${element.combinednotes}
+                  </td>
+                  <td>${actions}</td>
+               </tr>`;
+
+    return row;
+}
+
+function getValue(value) {
+    if (value && value.length > 0) return value;
+    return "";
+}
+
+function getBlankRow(colspan) {
+    let blankRow = `<tr>
+                     <td colspan="${colspan}" align="center"> No record(s) found</td>
+                    </tr>`;
+    return blankRow;
+}
+
+function showLoader() {
+    $(".loading").removeClass("hidden");
+}
+
+function hideLoader() {
+    $(".loading").addClass("hidden");
+}
